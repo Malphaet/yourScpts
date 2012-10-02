81d4e567439b58e700e3b234723f40fc319c7641
Malphaet <max.rigaut@gmail.com>
2012-10-02 13:46:45 +0200

    metagoofil 2.1
    tags: implemented,using

---
+++ Library/recipes/metagoofil.py
@@ -0,0 +1,10 @@
+from __recipe__ import recipe
+
+class metagoofil(recipe):
+	checktype="sha1"
+	checksum="73259b48016e659a5137dabbd633a603c53a0418"
+	homepage="http://code.google.com/p/metagoofil/"
+	download="http://metagoofil.googlecode.com/files/metagoofil-2.1_BH2011_Arsenal.tar.gz"
+	version='2.1'
+
+_install=metagoofil
