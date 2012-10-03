bffac080fa2fa8d651906ac85494af719bf8ea80
Malphaet <max.rigaut@gmail.com>
2012-10-03 02:37:00 +0200

    mdnsrecon - 0.1
    tags: implemented,using

---
+++ Library/recipes/mdnsrecon.py
@@ -0,0 +1,12 @@
+from __recipe__ import recipe
+
+class _install(recipe):
+	checktype="sha256"
+	checksum="b73704099019b679f6c228b0f529d19fe92e16e726a90235b9ecae65efc514f3"
+	homepage="https://github.com/darkoperator/MDNSRecon"
+	download="https://raw.github.com/darkoperator/MDNSRecon/3903c0b732fb57296aea72a9d6edc6086bc0d6a8/mdnsrecon.rb"
+	description=""
+	tags=""
+	version='0.1'
+	_deploy_name='mdnsrecon.rb'
+
