c64329e88df6fe6e56e8a15c83b72b266c84ac23
Malphaet <max.rigaut@gmail.com>
2012-10-03 02:34:01 +0200

    dnsrecon - 0.7.8
    tags: implemented,using

---
+++ Library/recipes/dnsrecon.py
@@ -0,0 +1,11 @@
+from __recipe__ import recipe
+
+class _install(recipe):
+	checktype="sha256"
+	checksum="fb63b2701cecd9ce945e6f777453f2ed443ca4338fa118a35a72844e1022a669"
+	homepage="https://github.com/darkoperator/dnsrecon"
+	download="https://github.com/darkoperator/dnsrecon/zipball/f545b46d96f17a914020666f95dddead6c0980f4"
+	description="DNSrecon is a simple tool writen for target enumeration during authorized penetration test engaments. This tool provides diferent methods for enumerating targets thru DNS service."
+	tags="pentest,DNS"
+	version='0.7.8'
+
