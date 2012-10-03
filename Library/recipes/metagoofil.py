82d6be19b0616ae024259fbda04946f2ab0050fb
Malphaet <max.rigaut@gmail.com>
2012-10-03 02:36:09 +0200

    metagoofil - 2.1
    tags: implemented,using

--- Library/recipes/metagoofil.py
+++ Library/recipes/metagoofil.py
@@ -5,6 +5,10 @@ class metagoofil(recipe):
 	checksum="73259b48016e659a5137dabbd633a603c53a0418"
 	homepage="http://code.google.com/p/metagoofil/"
 	download="http://metagoofil.googlecode.com/files/metagoofil-2.1_BH2011_Arsenal.tar.gz"
+	description="""\
+	Metagoofil is an information gathering tool designed for extracting metadata of public documents (pdf,doc,xls,ppt,docx,pptx,xlsx) belonging to a target company.
+	"""
+	tags="information gathering,harvesting"
 	version='2.1'
 
 _install=metagoofil
