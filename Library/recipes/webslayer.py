e5caaa53a5eef0e927de1f3f24b101a66a135b44
Malphaet <max.rigaut@gmail.com>
2012-10-03 02:41:24 +0200

    webslayer - 1.0a
    tags: implemented,using

---
+++ Library/recipes/webslayer.py
@@ -0,0 +1,13 @@
+from __recipe__ import recipe
+
+class _install(recipe):
+	checktype="sha256"
+	checksum="e250d7dcead4e7b0aa22c8b655a0b092d60174676da55994705918fa21bb3a50"
+	homepage="http://www.edge-security.com/webslayer.php"
+	download="https://github.com/Malphaet/webslayer/tarball/1.0a"
+	description="""
+WebSlayer is a tool designed for brute forcing Web Applications, it can be used for finding resources not linked (directories, servlets, scripts,files, etc), brute force GET and POST parameters, bruteforce Forms parameters (User/Password), Fuzzing, etc. The tools has a payload generator and an easy and powerful results analyzer.
+"""
+	tags="bruteforce,pentest,harvester,fuzzing"
+	version='1.0a'
+
