b0eb15ad658759ac5e9e5c62931b516b2a5ce0fa
Malphaet <max.rigaut@gmail.com>
2012-10-03 02:35:36 +0200

    host_list - 0.1
    tags: implemented,using

---
+++ Library/recipes/host_list.py
@@ -0,0 +1,12 @@
+from __recipe__ import recipe
+
+class _install(recipe):
+	checktype="sha1"
+	checksum="2fb2129f79dad6a8a9a9a346127814817caa403b"
+	homepage="http://ha.ckers.org/fierce"
+	download="http://ha.ckers.org/fierce/hosts.txt"
+	description="A nice big possible host list, for fierce and others"
+	tags="utils,dictionnary,host"
+	version='0.1'
+	_deploy_name='host.txt'
+
