604213e965a263a20fa166c69b7379ffd4d33d67
Malphaet <max.rigaut@gmail.com>
2012-10-03 02:35:16 +0200

    hosproxy - 1.0
    tags: implemented,using

---
+++ Library/recipes/hosproxy.py
@@ -0,0 +1,14 @@
+from __recipe__ import recipe
+
+class _install(recipe):
+	checktype="sha256"
+	checksum="fad14559a4ebf10cd70c6cb6416440e35b99d22e0d13fe3f919b6413c3c38376"
+	homepage="http://www.edge-security.com/hosproxy.php"
+	download="http://www.edge-security.com/soft/hosProxy-1.0.tar.gz"
+	description="""
+HoSProxy is a tool designed to let you access the web in a LAN without internet connection but with access to an E-mail system only. HoS means Http over SMTP, that is to say, using the E-mail system as http-proxy to navigate the web.
+	"""
+	tags="tunneling"
+	version='1.0'
+
+
